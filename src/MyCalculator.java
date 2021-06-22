db3b9cc088b3cc93d3603c0632c2661be76ba28a
Vika94 <vikuscha1994@mail.ru>
2021-06-22 23:47:11 +0300

    Initial commit

---
+++ src/MyCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MyCalculator {
+    public static void main(String[] args) {
+        for (; ; ) {
+            System.out.print("Введите 1-е число: ");
+            Scanner scan = new Scanner(System.in);
+            int num1 = scan.nextInt();
+            System.out.print("Введите 2-е число: ");
+            Scanner scan1 = new Scanner(System.in);
+            int num2 = scan1.nextInt();
+            System.out.print("Введите знак: ");
+            Scanner scan2 = new Scanner(System.in);
+            String a = scan2.nextLine();
+            if (a.equals("-"))
+                System.out.println("Ровно" + " " + (num1 - num2));
+            else if (a.equals("+"))
+                System.out.println("Ровно" + " " + (num1 + num2));
+            else if (a.equals("*"))
+                System.out.println("Ровно" + " " + (num1 * num2));
+            else if (a.equals("/"))
+                System.out.println("Ровно" + " " + (num1 / num2));
+        }
+    }
+}
+
